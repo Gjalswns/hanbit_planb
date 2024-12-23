caa220ac24bbf6d402553271b6f44b0398509338
gjalswns <huhmj1118@gmail.com>
2024-12-23 12:45:59 +0900

    initial css

--- project/system/system/settings.py
+++ project/system/system/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,6 +74,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'system.wsgi.application'
 
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'gameapp' / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -118,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
